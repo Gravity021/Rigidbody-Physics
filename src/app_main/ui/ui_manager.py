c95b0c2d436f9934dee547bcaebc2a9e3b7e2a4d
Gravity021 <hero45678@hotmail.com>
2025-01-05 18:21:53 +0000

    Update ui_manager.py

--- src/app_main/ui/ui_manager.py
+++ src/app_main/ui/ui_manager.py
@@ -2,6 +2,8 @@ from typing import Callable
 import pygame
 import pygame_gui
 
+from .ui_menu_bar import MenuBar
+from .ui_demo_window import DemoWindow
 class UIManager(pygame_gui.UIManager):
     def __init__(self, window_size: tuple[int, int], register_event_handler: Callable[[int, Callable[[pygame.Event], None]], None]) -> None:
         super().__init__(window_size)
