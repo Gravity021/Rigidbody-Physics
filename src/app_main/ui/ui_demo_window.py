36f9d23d8f8c5ebbaeff775656509cd3a7c933cd
Gravity021 <hero45678@hotmail.com>
2025-01-05 18:20:59 +0000

    Create UI Demo window

---
+++ src/app_main/ui/ui_demo_window.py
@@ -0,0 +1,31 @@
+import pygame
+import pygame_gui
+
+from ..core.event_manager import EventManager
+
+class DemoWindow(pygame_gui.elements.UIWindow):
+    def __init__(self, ui_manager: pygame_gui.UIManager):
+        super().__init__(pygame.Rect(20, 50, 220, 400), ui_manager, "Demo Window", resizable=True)
+
+        self.demo_button = pygame_gui.elements.UIButton(
+            pygame.Rect(10, 10, 200, 50), 
+            "Press Me!", 
+            self.ui_manager, 
+            command = lambda : print("Pressed! 🌼"), 
+            container = self, 
+            anchors = {'left': 'left', 'right': 'right'},
+            parent_element = self
+        )
+        self.drop_down_menu = pygame_gui.elements.UIDropDownMenu(
+            ["Pick an Option", "Item 1", "Item 2", "Item 3", "Item 4"], 
+            "Pick an Option", 
+            pygame.Rect(10, 70, 200, 50), 
+            self.ui_manager, 
+            container = self, 
+            parent_element = self,
+            anchors = {'left': 'left', 'right': 'right'},
+            expand_on_option_click = False
+        )
+    
+    def on_close_window_button_pressed(self):
+        super().hide()
--- src/app_main/ui/ui_manager.py
+++ src/app_main/ui/ui_manager.py
@@ -5,6 +5,8 @@ import pygame_gui
 class UIManager(pygame_gui.UIManager):
     def __init__(self, window_size: tuple[int, int], register_event_handler: Callable[[int, Callable[[pygame.Event], None]], None]) -> None:
         super().__init__(window_size)
+
+        self.test_window = DemoWindow(self)
     
     def toggle_visual_debug_mode(self, event) -> None:
         if event.key == pygame.K_F1:
