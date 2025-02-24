4b953cdf37d6b5924fe892d64af364676e05b60c
Gravity021 <hero45678@hotmail.com>
2025-02-24 20:03:35 +0000

    Update test methods for scene and physics manager

--- src/app_test/physics/test_physicsmanager.py
+++ src/app_test/physics/test_physicsmanager.py
@@ -9,18 +9,13 @@ from app_main.util.time import Time
 
 class PhysicsManagerTests(TestCase):
     def test_step_1(self):
-        Time.dt = 0.01
-
         obj = Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(1, -1), 1)
         obj1 = copy(obj)
         self.assertEqual(obj1.position, obj.position)
         self.assertEqual(obj1.velocity, obj.velocity)
         self.assertEqual(obj1.mass, obj.mass)
 
-        PhysicsManager.step([obj1])
-
-        print(f"Position: {obj1.position}")
-        print(f"Velocity: {obj1.velocity}")
+        PhysicsManager.step([obj1], 0.01)
 
         self.assertEqual(obj1.position.x, 1)
         self.assertEqual(obj1.position.y, 0.9995095)
@@ -29,8 +24,6 @@ class PhysicsManagerTests(TestCase):
         self.assertEqual(obj1.velocity.y, -0.0981)
     
     def test_step_2(self):
-        Time.dt = 0.01
-
         obj = Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(1, -1), 1)
         obj1 = copy(obj)
         self.assertEqual(obj1.position, obj.position)
@@ -38,10 +31,7 @@ class PhysicsManagerTests(TestCase):
         self.assertEqual(obj1.mass, obj.mass)
 
         for i in range(10):
-            PhysicsManager.step([obj1])
-
-        print(f"Position: {obj1.position}")
-        print(f"Velocity: {obj1.velocity}")
+            PhysicsManager.step([obj1], 0.01)
 
         self.assertEqual(obj1.position.x, 1)
         self.assertAlmostEqual(obj1.position.y, 0.95095)
@@ -51,7 +41,6 @@ class PhysicsManagerTests(TestCase):
     
     def test_step_3(self):
         delta = 10
-        Time.dt = 0.01
 
         obj = Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(1, -1), 1)
         obj1 = copy(obj)
@@ -59,11 +48,8 @@ class PhysicsManagerTests(TestCase):
         self.assertEqual(obj1.velocity, obj.velocity)
         self.assertEqual(obj1.mass, obj.mass)
 
-        for i in range(int(delta / Time.dt)):
-            PhysicsManager.step([obj1])
-
-        print(f"Position: {obj1.position}")
-        print(f"Velocity: {obj1.velocity}")
+        for i in range(int(delta / 0.01)):
+            PhysicsManager.step([obj1], 0.01)
 
         self.assertEqual(obj1.position.x, 1)
         self.assertAlmostEqual(obj1.position.y, -489.5)
@@ -73,7 +59,6 @@ class PhysicsManagerTests(TestCase):
     
     def test_step_4(self):
         delta = 100
-        Time.dt = 0.01
 
         obj = Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(1, -1), 1)
         obj1 = copy(obj)
@@ -81,11 +66,8 @@ class PhysicsManagerTests(TestCase):
         self.assertEqual(obj1.velocity, obj.velocity)
         self.assertEqual(obj1.mass, obj.mass)
 
-        for i in range(int(delta / Time.dt)):
-            PhysicsManager.step([obj1])
-
-        print(f"Position: {obj1.position}")
-        print(f"Velocity: {obj1.velocity}")
+        for i in range(int(delta / 0.01)):
+            PhysicsManager.step([obj1], 0.01)
 
         self.assertEqual(obj1.position.x, 1)
         self.assertAlmostEqual(obj1.position.y, 1 - 0.5 * 9.81 * delta ** 2)
@@ -95,7 +77,6 @@ class PhysicsManagerTests(TestCase):
 
     def test_step_5(self):
         delta = 325
-        Time.dt = 0.01
 
         obj = Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(1, -1), 1)
         obj1 = copy(obj)
@@ -103,11 +84,8 @@ class PhysicsManagerTests(TestCase):
         self.assertEqual(obj1.velocity, obj.velocity)
         self.assertEqual(obj1.mass, obj.mass)
 
-        for i in range(int(delta / Time.dt)):
-            PhysicsManager.step([obj1])
-
-        print(f"Position: {obj1.position}")
-        print(f"Velocity: {obj1.velocity}")
+        for i in range(int(delta / 0.01)):
+            PhysicsManager.step([obj1], 0.01)
 
         self.assertEqual(obj1.position.x, 1)
         self.assertAlmostEqual(obj1.position.y, 1 - 0.5 * 9.81 * delta ** 2)
--- src/app_test/physics/test_scene.py
+++ src/app_test/physics/test_scene.py
@@ -11,6 +11,7 @@ from app_main.maths.Vector2 import Vector2
 
 from app_main.physics.scene import Scene
 from app_main.physics.object import Object
+from app_main.util.debug import Debug
 
 class SceneTests(TestCase):
     
@@ -85,6 +86,170 @@ class SceneTests(TestCase):
         self.assertEqual(self.window.screen.get_at((64, 39)), Color([255, 255, 255, 255]))   # Test the top right corner is in the correct place
         self.assertEqual(self.window.screen.get_at((95, 24)), Color([255, 255, 255, 255]))   # Test the bottom left corner is in the correct place
         self.assertEqual(self.window.screen.get_at((95, 39)), Color([255, 255, 255, 255]))   # Test the bottom right corner is in the correct place
+
+    def test_render_5(self):
+        self.scene.objects = [Object(Object.ObjectType.CIRCLE, Vector2(1, 25 - 1), Vector2(0.5, -1), 1)]
+
+        self.scene.change_state(Scene.SceneState.PLAY)
+        self.scene._sim_objects[0].colour = [255, 0, 0, 255]
+
+        self.window.clear()
+        self.scene.render(self.window.screen)
+        self.window.update()
+
+        self.assertEqual(self.window.screen.get_at((16, 16)), Color([255, 0, 0, 255]))   # Test the centre is in the correct place
+
+        self.assertEqual(self.window.screen.get_at((16,  8)), Color([255, 0, 0, 255]))   # Test the top edge is in the correct place
+        self.assertEqual(self.window.screen.get_at((16, 23)), Color([255, 0, 0, 255]))   # Test the bottom edge is in the correct place
+        self.assertEqual(self.window.screen.get_at((8,  16)), Color([255, 0, 0, 255]))   # Test the left edge is in the correct place
+        self.assertEqual(self.window.screen.get_at((23, 16)), Color([255, 0, 0, 255]))   # Test the right edge is in the correct place
+
+    def test_render_6(self):
+        self.scene.objects = [Object(Object.ObjectType.CIRCLE, Vector2(1, 25 - 1), Vector2(0.5, -1), 1)]
+
+        self.scene.change_state(Scene.SceneState.PAUSE)
+        self.scene._sim_objects[0].colour = [255, 0, 0, 255]
+
+        self.window.clear()
+        self.scene.render(self.window.screen)
+        self.window.update()
+
+        self.assertEqual(self.window.screen.get_at((16, 16)), Color([255, 0, 0, 255]))   # Test the centre is in the correct place
+
+        self.assertEqual(self.window.screen.get_at((16,  8)), Color([255, 0, 0, 255]))   # Test the top edge is in the correct place
+        self.assertEqual(self.window.screen.get_at((16, 23)), Color([255, 0, 0, 255]))   # Test the bottom edge is in the correct place
+        self.assertEqual(self.window.screen.get_at((8,  16)), Color([255, 0, 0, 255]))   # Test the left edge is in the correct place
+        self.assertEqual(self.window.screen.get_at((23, 16)), Color([255, 0, 0, 255]))   # Test the right edge is in the correct place
+    
+    def test_change_state_edit_play(self):
+        scene = Scene()
+        scene.objects = [Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(0.5, -1), 1)]
+
+        self.assertEqual(scene.state, Scene.SceneState.EDIT)
+
+        scene.change_state(Scene.SceneState.PLAY)
+
+        self.assertEqual(scene.state, Scene.SceneState.PLAY)
+        self.assertEqual(scene.objects[0].object_type, scene._sim_objects[0].object_type)
+        self.assertEqual(scene.objects[0].position, scene._sim_objects[0].position)
+        self.assertEqual(scene.objects[0].dimensions, scene._sim_objects[0].dimensions)
+        self.assertEqual(scene.objects[0].velocity, scene._sim_objects[0].velocity)
+        self.assertEqual(scene.objects[0].force, scene._sim_objects[0].force)
+        self.assertEqual(scene.objects[0].colour, scene._sim_objects[0].colour)
+        self.assertEqual(scene.objects[0].mass, scene._sim_objects[0].mass)
+    
+    def test_change_state_play_pause(self):
+        scene = Scene()
+        scene.objects = [Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(0.5, -1), 1)]
+        scene.change_state(Scene.SceneState.PLAY)
+
+        self.assertEqual(scene.state, Scene.SceneState.PLAY)
+
+        scene.change_state(Scene.SceneState.PAUSE)
+
+        self.assertEqual(scene.state, Scene.SceneState.PAUSE)
+    
+    def test_change_state_play_edit(self):
+        scene = Scene()
+        scene.objects = [Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(0.5, -1), 1)]
+        scene.change_state(Scene.SceneState.PLAY)
+
+        self.assertEqual(scene.state, Scene.SceneState.PLAY)
+
+        scene.change_state(Scene.SceneState.EDIT)
+
+        self.assertEqual(scene.state, Scene.SceneState.EDIT)
+        self.assertEqual(scene._sim_objects, None)
+    
+    def test_change_state_pause_edit(self):
+        scene = Scene()
+        scene.objects = [Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(0.5, -1), 1)]
+        scene.change_state(Scene.SceneState.PAUSE)
+
+        self.assertEqual(scene.state, Scene.SceneState.PAUSE)
+
+        scene.change_state(Scene.SceneState.EDIT)
+
+        self.assertEqual(scene.state, Scene.SceneState.EDIT)
+        self.assertEqual(scene._sim_objects, None)
+    
+    def test_change_state_pause_play(self):
+        scene = Scene()
+        scene.objects = [Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(0.5, -1), 1)]
+        scene.change_state(Scene.SceneState.PAUSE)
+
+        self.assertEqual(scene.state, Scene.SceneState.PAUSE)
+
+        scene.change_state(Scene.SceneState.PLAY)
+
+        self.assertEqual(scene.state, Scene.SceneState.PLAY)
+
+    def test_change_state_edit_pause(self):
+        scene = Scene()
+        scene.objects = [Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(0.5, -1), 1)]
+        
+        self.assertEqual(scene.state, Scene.SceneState.EDIT)
+
+        scene.change_state(Scene.SceneState.PAUSE)
+
+        self.assertEqual(scene.state, Scene.SceneState.PAUSE)
+        self.assertEqual(scene.objects[0].object_type, scene._sim_objects[0].object_type)
+        self.assertEqual(scene.objects[0].position, scene._sim_objects[0].position)
+        self.assertEqual(scene.objects[0].dimensions, scene._sim_objects[0].dimensions)
+        self.assertEqual(scene.objects[0].velocity, scene._sim_objects[0].velocity)
+        self.assertEqual(scene.objects[0].force, scene._sim_objects[0].force)
+        self.assertEqual(scene.objects[0].colour, scene._sim_objects[0].colour)
+        self.assertEqual(scene.objects[0].mass, scene._sim_objects[0].mass)
+    
+    def test_change_state_invalid(self):
+        scene = Scene()
+        Debug._messages = []
+
+        scene.change_state(2)
+
+        self.assertEqual(scene.state, Scene.SceneState.EDIT)
+        self.assertEqual(len(Debug._messages), 1)
+        self.assertEqual(Debug._messages[0]["message"], "Invalid SceneState of 2")
+    
+    def test_step_1(self):
+        scene = Scene()
+        scene.objects = [Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(0.5, -1), 1)]
+
+        scene.change_state(Scene.SceneState.PAUSE)
+
+        self.assertEqual(scene._sim_objects[0].position, Vector2(1, 1))
+        self.assertEqual(scene._sim_objects[0].velocity, Vector2(0, 0))
+        self.assertEqual(scene._sim_objects[0].force, Vector2(0, 0))
+
+        scene.step(1)
+
+        # self.assertAlmostEqual(scene._sim_objects[0].position, Vector2(1, -3.905))
+        # self.assertAlmostEqual(scene._sim_objects[0].velocity, Vector2(0, -9.81))
+        self.assertEqual(scene._sim_objects[0].position.x, 1)
+        self.assertAlmostEqual(scene._sim_objects[0].position.y, -3.905)
+        self.assertEqual(scene._sim_objects[0].velocity.x, 0)
+        self.assertAlmostEqual(scene._sim_objects[0].velocity.y, -9.81)
+        self.assertEqual(scene._sim_objects[0].force, Vector2(0, 0))
+    
+    def test_step_2(self):
+        scene = Scene()
+        scene.objects = [Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(0.5, -1), 1)]
+
+        scene.change_state(Scene.SceneState.PAUSE)
+
+        self.assertEqual(scene._sim_objects[0].position, Vector2(1, 1))
+        self.assertEqual(scene._sim_objects[0].velocity, Vector2(0, 0))
+        self.assertEqual(scene._sim_objects[0].force, Vector2(0, 0))
+
+        scene.step(10)
+
+        # self.assertAlmostEqual(scene._sim_objects[0].position, Vector2(1, -489.5))
+        # self.assertAlmostEqual(scene._sim_objects[0].velocity, Vector2(0, -98.1))
+        self.assertEqual(scene._sim_objects[0].position.x, 1)
+        self.assertAlmostEqual(scene._sim_objects[0].position.y, -489.5)
+        self.assertEqual(scene._sim_objects[0].velocity.x, 0)
+        self.assertAlmostEqual(scene._sim_objects[0].velocity.y, -98.1)
+        self.assertEqual(scene._sim_objects[0].force, Vector2(0, 0))
     
     @classmethod
     def tearDownClass(self):
