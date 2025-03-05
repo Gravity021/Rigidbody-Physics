7bd40a7a4ab615b44ba2dd29a765392bdcae9713
Gravity021 <hero45678@hotmail.com>
2025-03-05 18:32:43 +0000

    Update Object to log errors rather than raising them

--- src/app_main/physics/object.py
+++ src/app_main/physics/object.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from app_main.util.debug import Debug
+
 from ..core.window import Window
 
 from ..maths.Vector2 import Vector2
@@ -56,7 +58,7 @@ class Object:
         if value in Object.ObjectType:
             self._object_type = value
         else:
-            raise ValueError(f"Invalid object type of {value}.")
+            Debug.log_error(f"Invalid object type of {value}.")
     
     @property
     def mass(self) -> float:
@@ -67,7 +69,7 @@ class Object:
         if value > 0:
             self._mass = value
         else:
-            raise ValueError(f"Mass must be a positive value. Value provided was {value}.")
+            Debug.log_error(f"Mass must be a positive value. Value provided was {value}.")
     
     @property
     def colour(self) -> list[int]:
@@ -92,21 +94,21 @@ class Object:
             if value.y != -1: y_invalid = True
             
             if x_invalid and y_invalid:
-                raise ValueError(f"Both x and y-components of dimensions for a circle are invalid. x-component must be positive and y-component must be '-1'. Values provided were {value.x} and {value.y}.")
+                Debug.log_error(f"Both x and y-components of dimensions for a circle are invalid. x-component must be positive and y-component must be '-1'. Values provided were {value.x} and {value.y}.")
             elif x_invalid:
-                raise ValueError(f"x-component of dimensions for a circle must be positive. Value provided was {value.x}.")
+                Debug.log_error(f"x-component of dimensions for a circle must be positive. Value provided was {value.x}.")
             elif y_invalid:
-                raise ValueError(f"y-component of dimensions for a circle must be '-1'. Value provided was {value.y}.")
+                Debug.log_error(f"y-component of dimensions for a circle must be '-1'. Value provided was {value.y}.")
 
         elif self._object_type == Object.ObjectType.RECTANGLE:
             if value.y <= 0: y_invalid = True
             
             if x_invalid and y_invalid:
-                raise ValueError(f"Both x and y-components of dimensions for a rectangle are invalid. Both components must be positive. Values provided were {value.x} and {value.y}.")
+                Debug.log_error(f"Both x and y-components of dimensions for a rectangle are invalid. Both components must be positive. Values provided were {value.x} and {value.y}.")
             elif x_invalid:
-                raise ValueError(f"x-component of dimensions for a rectangle must be positive. Value provided was {value.x}.")
+                Debug.log_error(f"x-component of dimensions for a rectangle must be positive. Value provided was {value.x}.")
             elif y_invalid:
-                raise ValueError(f"y-component of dimensions for a rectangle must be positive. Value provided was {value.y}.")
+                Debug.log_error(f"y-component of dimensions for a rectangle must be positive. Value provided was {value.y}.")
         
         self._dimensions = value
 
--- src/app_test/physics/test_object.py
+++ src/app_test/physics/test_object.py
@@ -2,52 +2,79 @@ from unittest import TestCase
 
 from app_main.maths.Vector2 import Vector2
 from app_main.physics.object import Object
+from app_main.util.debug import Debug
 
 class ObjectTests(TestCase):
-    def assertDoesntRaise(self, expected_exception: type[BaseException] | tuple[type[BaseException], ...], callable: (...), *_, **__) -> object:
-        raises_exception = False
-        try:
-            callable(*_, **__)
-        except expected_exception as ex:
-            print(ex)
-            raises_exception = True
+    def assertErrorLogged(self, msg: str = None):
+        error = False
+        for message in Debug._messages:
+            error |= message["level"] == "ERROR"
+            if error: break
         
-        self.assertFalse(raises_exception)
+        self.assertTrue(error, msg)
+        
+        Debug._messages = []  # Reset messages for next test case
+    
+    def assertNoErrorLogged(self, msg: str = None):
+        error = False
+        for message in Debug._messages:
+            error |= message["level"] == "ERROR"
+            if error: break
+        
+        self.assertFalse(error, msg)
+        
+        Debug._messages = []  # Reset messages for next test case
 
     def test_valid_constructor(self):
-        self.assertDoesntRaise(Exception, Object, Object.ObjectType.NONE, Vector2(1, 1), Vector2(1, 1), 1)
+        Object(Object.ObjectType.NONE, Vector2(1, 1), Vector2(1, 1), 1)
+        self.assertNoErrorLogged()
 
-        self.assertDoesntRaise(Exception, Object, Object.ObjectType.CIRCLE, Vector2(10, 10), Vector2(2, -1), 3, 2, (100, 175, 50))
+        Object(Object.ObjectType.CIRCLE, Vector2(10, 10), Vector2(2, -1), 3, 2, (100, 175, 50))
+        self.assertNoErrorLogged()
 
     def test_object_type_validation(self):
         # Test Invalid conditions
-        self.assertRaises(ValueError, Object, 2, Vector2(1, 1), Vector2(1, 1), 1)
-        self.assertRaises(ValueError, Object, 3, Vector2(1, 1), Vector2(1, 1), 1)
+        Object(2, Vector2(1, 1), Vector2(1, 1), 1)
+        self.assertErrorLogged()
+        Object(3, Vector2(1, 1), Vector2(1, 1), 1)
+        self.assertErrorLogged()
 
         # Test Valid conditions
-        self.assertDoesntRaise(Exception, Object, Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(1, -1), 1)
-        self.assertDoesntRaise(Exception, Object, Object.ObjectType.RECTANGLE, Vector2(1, 1), Vector2(1, 1), 1)
+        Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(1, -1), 1)
+        self.assertNoErrorLogged()
+        Object(Object.ObjectType.RECTANGLE, Vector2(1, 1), Vector2(1, 1), 1)
+        self.assertNoErrorLogged()
     
     def test_dimensions_validation(self):
         # Test Valid conditions
-        self.assertDoesntRaise(Exception, Object, Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(1, -1), 1)
-        self.assertDoesntRaise(Exception, Object, Object.ObjectType.RECTANGLE, Vector2(1, 1), Vector2(1, 1), 1)
+        Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(1, -1), 1)
+        self.assertNoErrorLogged()
+        Object(Object.ObjectType.RECTANGLE, Vector2(1, 1), Vector2(1, 1), 1)
+        self.assertNoErrorLogged()
 
         # Test Invalid conditions
-        self.assertRaises(ValueError, Object, Object.ObjectType.RECTANGLE, Vector2(1, 1), Vector2(0, 1), 1)
-        self.assertRaises(ValueError, Object, Object.ObjectType.RECTANGLE, Vector2(1, 1), Vector2(1, -1), 1)
+        Object(Object.ObjectType.RECTANGLE, Vector2(1, 1), Vector2(0, 1), 1)
+        self.assertErrorLogged()
+        Object(Object.ObjectType.RECTANGLE, Vector2(1, 1), Vector2(1, -1), 1)
+        self.assertErrorLogged()
 
-        self.assertRaises(ValueError, Object, Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(0, 1), 1)
-        self.assertRaises(ValueError, Object, Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(-1, 1), 1)
-        self.assertRaises(ValueError, Object, Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(1, 1), 1)
+        Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(0, 1), 1)
+        self.assertErrorLogged()
+        Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(-1, 1), 1)
+        self.assertErrorLogged()
+        Object(Object.ObjectType.CIRCLE, Vector2(1, 1), Vector2(1, 1), 1)
+        self.assertErrorLogged()
     
     def test_mass_validation(self):
         # Test Valid conditions
-        self.assertDoesntRaise(Exception, Object, Object.ObjectType.NONE, Vector2(1, 1), Vector2(1, 1), 1)
+        Object(Object.ObjectType.NONE, Vector2(1, 1), Vector2(1, 1), 1)
+        self.assertNoErrorLogged()
 
         # Test Invalid conditions
-        self.assertRaises(ValueError, Object, Object.ObjectType.NONE, Vector2(1, 1), Vector2(1, 1), 0)
-        self.assertRaises(ValueError, Object, Object.ObjectType.NONE, Vector2(1, 1), Vector2(1, 1), -10)
+        Object(Object.ObjectType.NONE, Vector2(1, 1), Vector2(1, 1), 0)
+        self.assertErrorLogged()
+        Object(Object.ObjectType.NONE, Vector2(1, 1), Vector2(1, 1), -10)
+        self.assertErrorLogged()
     
     def test_colour_validation(self):
         # Test Valid conditions
